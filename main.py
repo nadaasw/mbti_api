df0ccde0aa98cd3001b54b15dcf89d4ac994723e
nadaa <nadaa@ajou.ac.kr>
2025-02-08 14:49:35 +0900

    add cors

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 # CORS 설정
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://lucky-mbti.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
